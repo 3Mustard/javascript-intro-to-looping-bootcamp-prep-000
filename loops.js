89f1e2f406d4aa9db84f3f4a19a002043296d847
Matthew C <m23.cullen@gmail.com>
2019-07-19 15:47:03 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -25,10 +25,7 @@ function incrementVariable() {
 }
 
 function doWhileLoop(num){
-  do {
-    incrementVariable();
-    console.log("I run once regardless");
-  } while (incrementVariable() < num);
+ 
 }
 
 
